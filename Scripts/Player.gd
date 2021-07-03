8b9db31a56060b2910f32d568b1b1358c12230e8
Xulxuvar <teridax1306@gmail.com>
2021-07-02 21:09:47 -0500

    Knight Player
    we got a player, we got comboes, and we got movement

---
+++ Scripts/Player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var hitbox;
+var animationTree;
+var animationState;
+var movespeed = 5;
+var velocity = Vector2.ZERO
+var attack = 0;
+var timercount = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animationTree = get_node("AnimationTree")
+	animationState = animationTree.get("parameters/playback")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	input_vector = input_vector.normalized();
+	
+	if input_vector != Vector2.ZERO:
+		if(attack == 0):
+			animationState.travel("Running")
+		move_and_slide(input_vector*movespeed)
+	else:
+		if(attack == 0):
+			animationState.travel("Idle")
+	if Input.get_action_strength("ui_select"):
+		print(String(attack)+" "+String(timercount))
+	if Input.get_action_strength("ui_select") and attack == 0:
+		print("attacking")
+		if(timercount>0):
+			animationState.travel("Attack2")
+			attack = 2;
+		else:
+			animationState.travel("Attack1")
+			attack = 1;
+	if timercount>0:
+		timercount-=1;
+	pass
+
+func attackStopped(var attackID):
+	print("attack stopped")
+	
+	if attackID == 1:
+		timercount = 20
+	attack = 0
+	return
