188e3e046f6ca4132c4fa216e47cd48587e5b305
Xulxuvar <teridax1306@gmail.com>
2021-07-04 03:01:45 -0500

    Player is Feature Complete
    Job's done

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,10 +7,19 @@ extends KinematicBody2D
 var hitbox;
 var animationTree;
 var animationState;
+var animationPlayer;
+var rotate;
 var movespeed = 50;
 var velocity = Vector2.ZERO
+var feet;
+var rotateable = true;
+
+var holding = false;
+var attackP = 0;
 var attack = 0;
 var timercount = 0;
+var holdTolerance = 30;
+var holdCount = -1;
 
 var hBarFill;
 var hBarFrame;
@@ -24,8 +33,11 @@ var iFramesM = 60;
 func _ready():
 	hBarFill = get_node("Camera2D/Control/HealthFill")
 	hBarFrame = get_node("Camera2D/Control/HealthFrame")
+	animationPlayer= get_node("AnimationPlayer")
 	animationTree = get_node("AnimationTree")
+	rotate = get_node("RotationPivot (Sneed)")
 	animationState = animationTree.get("parameters/playback")
+	feet = get_node("RotationPivot (Sneed)/AnimatedSprite/Feet")
 	updateHealth();
 	pass # Replace with function body.
 
@@ -37,22 +49,22 @@ func _process(delta):
 	input_vector = input_vector.normalized();
 	
 	if input_vector != Vector2.ZERO:
-		if(attack == 0):
-			animationState.travel("Running")
-		move_and_slide(input_vector*movespeed)
-	else:
-		if(attack == 0):
-			animationState.travel("Idle")
-	if Input.get_action_strength("ui_select"):
-		print(String(attack)+" "+String(timercount))
-	if Input.get_action_strength("ui_select") and attack == 0:
-		print("attacking")
-		if(timercount>0):
-			animationState.travel("Attack2")
-			attack = 2;
+		if(rotateable):
+			if(input_vector.x>0):
+				rotate.scale.x = 1;
+			else:
+				rotate.scale.x = -1;
+		if(Input.get_action_strength("ui_down")):
+			feet.play("Sprint")
+			move_and_slide(input_vector*2*movespeed)
 		else:
-			animationState.travel("Attack1")
-			attack = 1;
+			feet.play("Walk")
+			move_and_slide(input_vector*movespeed)
+	else:
+		feet.play("Idle")
+	
+	checkAttack();
+	
 	if iFrames>0:
 		iFrames-=1;
 			
@@ -60,10 +72,49 @@ func _process(delta):
 		timercount-=1;
 	pass
 
+func checkAttack():
+	
+	if Input.is_action_just_pressed("ui_select") and attack ==0:
+		holdCount = 0;
+	
+	if holdCount != -1:
+		holdCount+=1;
+		if holdCount >=holdTolerance and attack == 0:
+			attack = 4;
+			animationState.travel("HoldAttack")
+			rotateable = false;
+	
+	if Input.is_action_just_released("ui_select"):
+		if holdCount <holdTolerance:
+			rotateable = false;
+			if( Input.get_action_strength("ui_up")):
+				animationState.travel("AttackOverhead")
+				attack = 4;
+			else:
+				if(timercount>0):
+					if(attackP == 1):
+						animationState.travel("AttackDown")
+						attack = 2;
+					else:
+						animationState.travel("AttackStab")
+						attack = 3;
+				else:
+					animationState.travel("AttackUp")
+					attack = 1;
+		holdCount = -1;
+			
+func nowRotate():
+	rotateable = true;
+
+func die():
+	#Game over goes here
+	return;
+
 func attackStopped(var attackID):
+	attackP = attackID;
 	print("attack stopped")
 	
-	if attackID == 1:
+	if attackID == 1 or attackID == 2:
 		timercount = 20
 	attack = 0
 	return
@@ -73,12 +124,23 @@ func updateHealth():
 	print(-8-(232*(1-health/healthM)))
 	hBarFill.position.x = -240+(232*health/healthM);
 
+func holdingAttack():
+	print("Checking if holding")
+	if not Input.get_action_strength("ui_select"):
+		holding = false
+		rotateable = false;
+		attackStopped(4)
+		animationState.travel("Idle")
+	
+
 func _on_Damage_area_entered(area):
 	var damage = area.get_parent().get_parent().get_parent().getAttack()
 	
 	if iFrames == 0:
 		health-=damage
 		print(health)
+		if health <=0:
+			die()
 		updateHealth();
 		iFrames = iFramesM
 	
